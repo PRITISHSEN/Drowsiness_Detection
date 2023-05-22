4a4dc051279b0b9dfbeafd7db1a01556666f20ff
PRITISHSEN <78251102+PRITISHSEN@users.noreply.github.com>
2023-05-22 17:39:29 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,7 @@
+account_sid = 'AC290bd60b37e0847ea0cb3d33e833d5cc'
+auth_token = 'ac00615dd97d92ce6295bbe2d55d031f'
+
+# Enter the twilio no.
+twilio_number = '+17695538456'
+# Enter you phone no.
+target_number = '+919937103783'
